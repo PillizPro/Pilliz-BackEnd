e036e287955974da8ce96b0e0042fcd312b15d77
noaleclaire <noaleclaire@users.noreply.github.com>
2023-10-22 14:39:49 +0000

    UPDATE: Lint files

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,45 +6,58 @@ import bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   async register(registerDto: CreateUserDto) {
-    const hashedPassword = await this._hashPassword(registerDto.password);
-    const userDtoWithHashedPassword = { ...registerDto, password: hashedPassword };
-    return await this.userService.createUser(userDtoWithHashedPassword);
+    const hashedPassword = await this._hashPassword(registerDto.password)
+    const userDtoWithHashedPassword = {
+      ...registerDto,
+      password: hashedPassword,
+    }
+    return await this.userService.createUser(userDtoWithHashedPassword)
   }
 
   private async _hashPassword(password: string): Promise<string> {
-    const saltRounds = 10;
-    const hash = await bcrypt.hash(password, saltRounds);
-    return hash;
+    const saltRounds = 10
+    const hash = await bcrypt.hash(password, saltRounds)
+    return hash
   }
 
   async login(loginDto: LoginDto) {
-    const user = await this.userService.findByEmail(loginDto);
+    const user = await this.userService.findByEmail(loginDto)
 
     if (!user) {
-      throw new UnauthorizedException('Invalid email or password.');
+      throw new UnauthorizedException('Invalid email or password.')
     }
 
-    const isPasswordMatch = await this._verifyPassword(loginDto.password, user.password);
+    const isPasswordMatch = await this._verifyPassword(
+      loginDto.password,
+      user.password
+    )
 
     if (!isPasswordMatch) {
-      throw new UnauthorizedException('Invalid email or password.');
+      throw new UnauthorizedException('Invalid email or password.')
     }
 
     if (user.banned === 'banned') {
-      return { status: 'banned' };
+      return { status: 'banned' }
     }
 
     if (user.role === 'admin') {
-      return { status: 'success', isAdmin: true };
+      return { status: 'success', isAdmin: true }
     }
-    return { status: 'success', isAdmin: false, username: user.name, email: user.email };
+    return {
+      status: 'success',
+      isAdmin: false,
+      username: user.name,
+      email: user.email,
+    }
   }
 
-  private async _verifyPassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
-    return await bcrypt.compare(plainPassword, hashedPassword);
+  private async _verifyPassword(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(plainPassword, hashedPassword)
   }
 }
