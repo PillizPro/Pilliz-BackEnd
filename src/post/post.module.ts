65f8e167ecf4ffd3aa80154027b6fb1ea7aa84d5
noaleclaire <noaleclaire@users.noreply.github.com>
2023-11-13 16:22:32 +0000

    [+] Lint files

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common'
 import { PostController } from './post.controller'
 import { PostService } from './post.service'
 
-
 @Module({
   controllers: [PostController],
   providers: [PostService],
 })
-export class PostModule { }
+export class PostModule {}
