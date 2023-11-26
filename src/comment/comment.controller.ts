ee27c2d8e16bd29ced75eb01e13d7ae06755cf7d
Enzo Vignal <evignal@localhost.localdomain>
2023-11-26 16:57:50 +0100

    {+} Suppresion de print de debugage

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -15,35 +15,6 @@ export class CommentController {
 
   @Get('findCommentsOnPost:id')
   async findCommentsOnPost(@Param('id') postId: string) {
-    console.log(postId);
     return await this.commentService.findCommentsOnPost(postId)
   }
-
-
-
-
-  // @Get('findallpost')
-  // async findAllPosts() {
-  //   return await this.postService.findAllPosts()
-  // }
-
-  // @Get('find20LastsPosts')
-  // async find20LastsPosts() {
-  //   return await this.postService.find20LastsPosts()
-  // }
-
-  // @Get('findPostInfo:id')
-  // async findPostById(@Param('id') postId: string) {
-  //   return await this.postService.findPostById(postId)
-  // }
-
-  // @Get('find20RecentsPosts:date')
-  // async find20RecentsPosts(@Param('date') dateString: Date) {
-  //   return await this.postService.find20RecentsPosts(dateString);
-  // }
-
-  // @Get('find20OlderPosts:date')
-  // async find20OlderPosts(@Param('date') dateString: Date) {
-  //   return await this.postService.find20OlderPosts(dateString);
-  // }
 }
