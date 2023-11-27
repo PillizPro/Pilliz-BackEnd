ee8e017d2b23ae73a9e64b2b888530378f23a0b8
noaleclaire <noa.leclaire@epitech.eu>
2023-11-27 16:37:51 +0100

    [+] add product entity [~] return array of product entities instead of products directly from prisma

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ProductEntity } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
@@ -7,9 +8,12 @@ export class ProductService {
 
   async findAllProducts() {
     try {
-      const products = await this.prismaService.product.findMany({})
+      const products = await this.prismaService.product.findMany()
 
-      return products
+      const entitiesProducts = products.map((product) => {
+        new ProductEntity(product)
+      })
+      return entitiesProducts
     } catch (error) {
       console.error(error)
       throw new Error('An error occured when getting products')
