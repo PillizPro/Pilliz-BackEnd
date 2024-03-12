9c95870f8194d9f636544fe1c27b0c0f21d57c15
noaleclaire <noa.leclaire@epitech.eu>
2024-03-12 04:29:34 +0100

    [+]: add jwt access token [+]: beginning of implementing jwt refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { LoginDto } from './dto/login.dto'
 import { CreateUserDto } from 'src/user/dto/create-user.dto'
-import { ResetPassword } from './dto/reset-password.dto'
+import { ResetPasswordDto } from './dto/reset-password.dto'
 import { UserService } from 'src/user/user.service'
 import { PrismaService } from 'src/prisma/prisma.service'
 import * as bcrypt from 'bcrypt'
+import { UserEntity } from 'src/user/entities/user.entity'
+import { JwtService } from '@nestjs/jwt'
+import { ITokenPayload } from './strategy/jwt.strategy'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly userService: UserService
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
   ) {}
 
   async register(registerDto: CreateUserDto) {
@@ -22,35 +28,28 @@ export class AuthService {
     return await this.userService.createUser(userDtoWithHashedPassword)
   }
 
-  private async _hashPassword(password: string): Promise<string> {
-    const saltRounds = 10
-    const hash = await bcrypt.hash(password, saltRounds)
-    return hash
-  }
-
-  async login(loginDto: LoginDto) {
+  async validateUser(loginDto: LoginDto) {
     const user = await this.userService.findByEmail(loginDto)
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid email or password.')
-    }
+    if (!user) return null
 
     const isPasswordMatching = await this._verifyPassword(
       loginDto.password,
       user.password
     )
 
-    if (!isPasswordMatching) {
-      throw new UnauthorizedException('Invalid email or password.')
-    }
+    if (!isPasswordMatching) return null
 
-    if (user.banned === 'banned') {
-      return { status: 'banned' }
-    }
+    if (user.banned === 'banned') return { status: 'banned' }
 
-    if (user.role === 'admin') {
-      return { status: 'success', isAdmin: true }
-    }
+    if (user.role === 'admin')
+      return {
+        status: 'success',
+        isAdmin: true,
+        email: user.email,
+        id: user.id,
+      }
+
     return {
       status: 'success',
       isAdmin: false,
@@ -64,32 +63,41 @@ export class AuthService {
     }
   }
 
-  private async _verifyPassword(
-    plainPassword: string,
-    hashedPassword: string
-  ): Promise<boolean> {
-    return await bcrypt.compare(plainPassword, hashedPassword)
+  async login(user: any) {
+    if (user.status === 'banned') return user
+    const payload: ITokenPayload = { sub: user.id, email: user.email }
+    return {
+      access_token: this.jwtService.sign(payload, {
+        secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
+        expiresIn: `${this.configService.get(
+          'JWT_ACCESS_TOKEN_EXPIRATION_TIME'
+        )}s`,
+      }),
+      refresh_token: this.jwtService.sign(payload, {
+        secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+        expiresIn: `${this.configService.get(
+          'JWT_REFRESH_TOKEN_EXPIRATION_TIME'
+        )}s`,
+      }),
+    }
   }
 
-  async resetPassword(resetPassDto: ResetPassword) {
+  async resetPassword(resetPassDto: ResetPasswordDto) {
     const { email, oldPassword, newPassword } = resetPassDto
 
     // console.log(email, oldPassword, newPassword)
 
     const user = await this.userService.findByEmail({ email })
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid email.')
-    }
+    if (!user) throw new UnauthorizedException('Invalid email.')
 
-    const isPasswordMatch = await this._verifyPassword(
+    const isPasswordMatching = await this._verifyPassword(
       oldPassword,
       user.password
     )
 
-    if (!isPasswordMatch) {
+    if (!isPasswordMatching)
       throw new UnauthorizedException('Invalid old password.')
-    }
 
     const hashedPassword = await this._hashPassword(newPassword)
 
@@ -98,6 +106,19 @@ export class AuthService {
       data: { password: hashedPassword },
     })
 
-    return reseting
+    return new UserEntity(reseting)
+  }
+
+  private async _hashPassword(password: string): Promise<string> {
+    const saltRounds = 10
+    const hash = await bcrypt.hash(password, saltRounds)
+    return hash
+  }
+
+  private async _verifyPassword(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(plainPassword, hashedPassword)
   }
 }
--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty } from 'class-validator'
 
-export class ResetPassword {
+export class ResetPasswordDto {
   @IsNotEmpty()
   readonly email: string
 
