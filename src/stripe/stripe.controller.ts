8ef72f21ddd03afcfcc3114063aa954b6c780327
noaleclaire <72016693+noaleclaire@users.noreply.github.com>
2024-10-11 05:50:02 +0000

    [+] Lint files

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,17 +1,17 @@
 // src/stripe/stripe.controller.ts
 
-import { ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Post } from '@nestjs/common';
-import { StripeService } from './stripe.service';
+import { ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Post } from '@nestjs/common'
+import { StripeService } from './stripe.service'
 
 @ApiTags('Stripe')
 @Controller('stripe')
 export class StripeController {
-  constructor(private readonly stripeService: StripeService) { }
+  constructor(private readonly stripeService: StripeService) {}
 
   @Post('create-product')
   async createProduct(@Body() productData: any) {
-    return await this.stripeService.createProduct(productData);
+    return await this.stripeService.createProduct(productData)
   }
 
   // @Post('payment')
